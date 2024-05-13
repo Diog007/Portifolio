62ebff93b325a7fb2074b2561e15b6534ce77e71
Diog007 <diogo.mz123@gmail.com>
2024-05-13 14:46:01 -0300

    Created header and main section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.component';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { MainAnimationComponent } from '../../shared/main-animation/main-animation.component';
+import { MainSectionComponent } from '../../shared/main-section/main-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BtnPrimaryComponent, HeaderComponent],
+  imports: [BtnPrimaryComponent, HeaderComponent, MainAnimationComponent, MainSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
