c3d1ae2780fcb8daff06f7571b8ed77fb82f8e3d
Diog007 <diogo.mz123@gmail.com>
2024-05-16 11:41:37 -0300

    Implemented AOS library - Animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormacaoComponent } from './components/formacao/formacao.component';
 import { FundoComponent } from './components/fundo/fundo.component';
 import { SobreComponent } from './components/sobre/sobre.component';
+import Aos, * as AOS from 'aos'
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +16,12 @@ import { SobreComponent } from './components/sobre/sobre.component';
 })
 export class AppComponent {
   title = 'Portifolio-web';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+      window.addEventListener('load', AOS.refresh);
+    }
+  }
 }
