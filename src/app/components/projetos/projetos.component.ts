09b8e87a1cabff930880c10a007c1bf4d509c15f
Diog007 <diogo.mz123@gmail.com>
2024-05-17 13:54:31 -0300

    Projects component created

---
+++ src/app/components/projetos/projetos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.component';
+
+@Component({
+  selector: 'app-projetos',
+  standalone: true,
+  imports: [BtnPrimaryComponent],
+  templateUrl: './projetos.component.html',
+  styleUrl: './projetos.component.scss'
+})
+export class ProjetosComponent {
+
+}
