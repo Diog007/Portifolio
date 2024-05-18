a499d0411163ee758c3eb9ecc950377081556fd2
Diog007 <diogo.mz123@gmail.com>
2024-05-18 17:09:25 -0300

    Adjusting the position of the training component

--- src/app/components/formacao/formacao.component.ts
+++ src/app/components/formacao/formacao.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-formacao',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './formacao.component.html',
   styleUrl: './formacao.component.scss'
 })
